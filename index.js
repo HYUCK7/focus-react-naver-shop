8c426bc4d7d3067c434de24729c748d72e976466
HYUCK7 <iqeq12191219@gmail.com>
2022-02-14 13:59:28 +0900

    2022-02-14 13:58 Hello

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './hello/Hello';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
