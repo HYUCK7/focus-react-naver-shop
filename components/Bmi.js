f95726bb60df31e5386085f70f06263dfddf6365
HYUCK7 <iqeq12191219@gmail.com>
2022-02-24 17:05:54 +0900

    2022-02-24 17:05 정상입니다.

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react"
 import Layout from "../containers/Layout";
-
+import axios from 'axios';
 export default function Bmi(){
-    const[name,setName] = useState()
-    const[length, setLength] = useState()
-    const[weight, setWeight] = useState()
+    const[name,setName] = useState('')
+    const[length, setLength] = useState(0.0)
+    const[weight, setWeight] = useState(0.0)
     const sum =()=>{
         let name = document.getElementById('name').value
         console.log(`이름: ` + name)
@@ -15,7 +15,21 @@ export default function Bmi(){
         setName(name)
         setLength(length)
         setWeight(weight)
-        
+    }
+    const getBmi =async (e) =>{
+        e.preventDefault()
+        alert(` 버튼 클릭 `)
+        try{
+            setName(`홍길동`)
+            setLength(180.5)
+            setWeight(80.5)
+            await axios.get(`http://localhost:8080/member/bmi/홍길동/180.5/70`)
+            .then((res)=>{
+                    alert(`답장이 도착했습니다. [내용] ${JSON.stringify(res.data)}`)
+            })
+        }catch(error){
+                alert(`답장이 도착했습니다.`)
+        }
     }
     return <Layout><h1>BMI</h1>
         <div>
@@ -33,6 +47,7 @@ export default function Bmi(){
             <button onClick={()=>{}}>이름</button>
             <button onClick={()=>{}}>키</button>
             <button onClick={()=>{}}>몸무게</button>
+            <input type = "button" onClick ={getBmi} value ="BMI 체크"/><br/>
             <button onClick={()=>{sum()}}>결과 출력</button>
             <div>결과 :{name}, {length}, {weight}</div>
         </div>
